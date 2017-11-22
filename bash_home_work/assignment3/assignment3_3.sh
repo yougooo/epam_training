150fe6695b0acb17c0a63c0b6abbd17fda6a0715
Alex <yougooo10@gmail.com>
2017-11-22 19:39:26 +0200

    added binary check function

--- bash_home_work/assignment3/assignment3_3.sh
+++ bash_home_work/assignment3/assignment3_3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 [ $# -eq 0  ] && path=$PWD || path=$1
 
 base=${path##*/}     # for take local path in function,
@@ -7,16 +8,31 @@ abs=${path%%$base}   # 'base' it's start directory name
                      # 'abs' it's other part of absolute
 		     # path without 'base'. 
 
+bincheck () {
+	if [[ -s $1 ]]                
+	then
+		if [[ `file --mime-encoding $1 | awk '($2=="binary") {print $2}'` ]]
+		then
+			echo 'is binary'
+		else
+			echo "has: `cat $1 | wc -l` lines"
+		fi	
+        else
+		echo 'has: 0 lines'
+	fi 
+            }
+
+
 tree_like () {
 	for file in $1/*
 	do
-	    [ -f $file ] && echo "File: ./${file##$abs} file has: `cat $file | wc -l` lines"
-	    [ -d $file ] && echo "Directory: ./${file##$abs}" && tree_like $file  
+	    [ -f $file ] && echo "File: ${file##$abs} `bincheck $file`"
+	    [ -d $file ] && echo "Directory: ${file##$abs}" && tree_like $file  
 	done
-}
+             }
 
 echo "Full path is $path"
-echo Directory: ./${path##$abs}
+echo Directory: ${path##$abs}
 
 temp=`tree_like $path`
 
