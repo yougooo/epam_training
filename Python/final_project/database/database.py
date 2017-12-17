e35add46ad086fff40ef07d6fe655306b5f246db
Alex <yougooo10@gmail.com>
2017-12-17 16:03:38 +0200

    added fields to database

--- Python/final_project/database/database.py
+++ Python/final_project/database/database.py
@@ -9,47 +9,51 @@ connection_row = "dbname=library user=alex password=95qaz26plm"
 
 with psycopg2.connect(connection_row) as connect:
     with connect.cursor() as cursor:
-        cursor.execute("DROP TABLE IF EXISTS books_list")
-        cursor.execute("DROP TABLE IF EXISTS authors")
+        cursor.execute("DROP TABLE IF EXISTS book_entry")
+        cursor.execute("DROP TABLE IF EXISTS author")
         cursor.execute("DROP TABLE IF EXISTS genre")
-        cursor.execute("DROP TABLE IF EXISTS books")
+        cursor.execute("DROP TABLE IF EXISTS book")
 
         # Create authors table for e-library app
-        cursor.execute("CREATE TABLE authors(" +
-                       "id SERIAL PRIMARY KEY," +
+        cursor.execute("CREATE TABLE author(" +
+                       "author_id SERIAL PRIMARY KEY," +
                        "first_name VARCHAR(25) NOT NULL," +
                        "last_name VARCHAR(25) NOT NULL," +
                        "born_date DATE NOT NULL," +
                        "country VARCHAR(30)," +
+                       "update_date DATE NOT NULL default CURRENT_DATE,"+
                        "UNIQUE(first_name, last_name, born_date))")
 
-        print('Table authors. OK')
+        print('Table author... OK')
 
         cursor.execute("CREATE TABLE genre(" +
-                        "id SERIAL PRIMARY KEY," +
+                        "genre_id SERIAL PRIMARY KEY," +
                         "genre VARCHAR(30) NOT NULL)")
 
-        print('Table genre. OK')
+        print('Table genre... OK')
 
-        cursor.execute("CREATE TABLE books(" +
-                       "id SERIAL PRIMARY KEY," +
+        cursor.execute("CREATE TABLE book(" +
+                       "book_id SERIAL PRIMARY KEY," +
                        "book_name VARCHAR(100) NOT NULL," +
                        "publication_date DATE NOT NULL," +
                        "description TEXT," +
+                       "update_date DATE NOT NULL default CURRENT_DATE,"+
                        "UNIQUE(book_name, publication_date))")
 
-        print('Table books. OK')
+        print('Table book... OK')
 
-        cursor.execute("CREATE TABLE books_list(" +
-                       "author_id int4 REFERENCES authors (id) " +
+        cursor.execute("CREATE TABLE book_entry(" +
+                       "library_entry SERIAL," +
+                       "author_id int4 REFERENCES author (author_id) " +
                        "ON UPDATE CASCADE ON DELETE CASCADE," +
-                       "book_id int4 REFERENCES books (id) " +
+                       "book_id int4 REFERENCES book (book_id) " +
                        "ON UPDATE CASCADE ON DELETE CASCADE," +
-                       "genre_id int4 REFERENCES genre (id) " +
+                       "genre_id int4 REFERENCES genre (genre_id) " +
                        "ON UPDATE CASCADE ON DELETE CASCADE," +
-                       "CONSTRAINT list_id PRIMARY KEY (author_id, book_id))")
+                       "CONSTRAINT list_id PRIMARY KEY (author_id, book_id),"+
+                       "published BOOLEAN NOT NULL default FALSE)")
 
-        print('Table books_list. OK')
+        print('Table book_entry... OK')
 
         connect.commit()
         cursor.close()
